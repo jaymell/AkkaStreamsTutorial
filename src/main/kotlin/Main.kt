d6ddbb7fc6c1a8eaec6743fdc7686f9f5371e0b9
James Lucas <jamesmlucas@gmail.com>
2017-08-10 22:47:07 -0500

    exceptions branch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,103 +1,43 @@
 import akka.stream.*;
 import akka.stream.javadsl.*;
-import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.stream.impl.PublisherSink
-import akka.util.ByteString;
-
-import java.nio.file.Paths;
-import java.math.BigInteger;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import akka.stream.Supervision
 
-import scala.concurrent.duration.Duration;
 
-import twitter4j.*
-import twitter4j.conf.ConfigurationBuilder
-import java.lang.Exception
-
-class MyStatusListener: StatusListener {
-   override fun onStatus(status: Status) {
-       println(status.getText())
-   }
-
-    override fun onTrackLimitationNotice(numberOfLimitedStatuses: Int) {
-        println(numberOfLimitedStatuses)
-    }
-
-    override fun onDeletionNotice(statusDeletionNotice: StatusDeletionNotice?) {
-        println(statusDeletionNotice)
-    }
-
-    override fun onStallWarning(warning: StallWarning?) {
-        println(warning)
-    }
-
-    override fun onException(ex: Exception?) {
-        println(ex)
-    }
-
-    override fun onScrubGeo(userId: Long, upToStatusId: Long) {
-        println(userId)
-    }
+fun decider(exc: Throwable): Supervision.Directive {
+  if (exc is ArithmeticException) {
+      println("Exception encountered")
+      return Supervision.resume()
+  }
+  else {
+      return Supervision.stop()
+  }
 }
 
-class MyRawListener: RawStreamListener {
-	override fun onMessage(msg: String) {
-		return msg
-	}
-    override fun onException(ex: Exception?) {
-        return ex
-    }
-}
 
 fun main(args: Array<String>) {
+
     val system: ActorSystem = ActorSystem.create("QuickStart")
-    val materializer: Materializer = ActorMaterializer.create(system)
 
-    val configBuilder: ConfigurationBuilder = ConfigurationBuilder()
-            .setOAuthConsumerKey(appKey)
-            .setOAuthConsumerSecret(appSecret)
-            .setOAuthAccessToken(accessToken)
-            .setOAuthAccessTokenSecret(accessTokenSecret)
+    val mat: Materializer = ActorMaterializer.create(ActorMaterializerSettings.create(system)
+            .withSupervisionStrategy(::decider), system)
 
-    fun source(): Source<String, NotUsed> {
-        val twitterStream: TwitterStream = TwitterStreamFactory(configBuilder.build()).getInstance()
-        val sink: Sink = Sink<String, NotUsed>()
-        Twitter4JHelper.addStatusListner(twitterStream, MyStatusListener())
-        twitterStream.sample()
-        val src = Source.actorRef<String>(1000, OverflowStrategy.fail())
+    val flow: Flow<Int, Int, NotUsed> =
+        Flow.of(Int::class.java)
+            .filter{ 100 / it < 50 }
+            .map{ 100 / (5-it) }
+
+    val source: Source<Int, NotUsed> = Source.from(listOf(0, 1, 2, 3, 4, 5))
+            .via(flow)
+
+    val sink = Sink.fold<Int, Int>(0) { acc, elem ->
+        val ret = acc + elem
+        println("acc: ${ret}")
+        ret
     }
 
-	// raw stream
-    //Twitter4JHelper.addRawListner(twitterStream, MyRawListener())
-	
-    // apply arbitrary filter
-    //Twitter4JHelper.addStatusListner(twitterStream, MyStatusListener())
-    // val tweetFilterQuery: FilterQuery = FilterQuery()
-    // tweetFilterQuery.track(arrayOf("trump"))
-    // twitterStream.filter(tweetFilterQuery)
-
-//    val source: Source<Int, NotUsed> = Source.range(1, 100)
-//    val factorials = source
-//        .scan(BigInteger.ONE) { acc, next ->
-//            acc.multiply(BigInteger.valueOf(next.toLong()))
-//        }
-    // instead of this:
-//    val result: CompletionStage<IOResult> = factorials
-//        .map{ ByteString.fromString(it.toString() + "\n") }
-//        .runWith(FileIO.toPath(Paths.get("factorials.txt")), materializer)
-
-    // make a reusable sink:
-//    fun lineSink(filename: String): Sink<String, CompletionStage<IOResult>> {
-//        return Flow.of(String::class.java)
-//            .map{ ByteString.fromString(it.toString() + "\n") }
-//            .toMat(FileIO.toPath(Paths.get(filename)), Keep.right())
-//    }
-//
-//    factorials.map(BigInteger::toString).runWith(lineSink("factorial2.txt"), materializer)
-
-
+    val result: CompletionStage<Int> = source.runWith(sink, mat)
 }
+
